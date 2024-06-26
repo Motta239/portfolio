c63a75296664df1421b568cf3b00351570421010
moti yosef <motiyosef@Mac.lan>
2024-06-26 14:57:08 +0300

    Describe your changes here

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -23,37 +23,35 @@ const ProjectCard: React.FC<Props> = ({
 
   return (
     <div
-      className="relative rounded-lg shadow-lg border border-[#2A0E61]  h-[500px] md:h-[600px] overflow-hidden"
+      className="relative rounded-lg shadow-lg border border-[#2A0E61] h-[500px] lg:h-[600px] overflow-hidden"
       style={{
         backgroundImage: `url(${src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="absolute bottom-0 p-3 bg-black bg-opacity-70">
-        <div className="relative z-10 flex flex-col justify-between flex-grow p-4">
-          <div>
-            <h1 className="text-2xl font-semibold text-white">{title}</h1>
-            <div className="mt-4 text-gray-100">{description}</div>
-            <div className="flex flex-wrap space-x-2">
-              {features.map((feature, index) => (
-                <div
-                  key={index}
-                  className="bg-blue-600 flex m-1 px-3 py-1  items-center justify-center bg-opacity-80 hover:bg-opacity-40 rounded-xl text-white"
-                >
-                  {feature}
-                </div>
-              ))}
-            </div>
+      <div className="absolute bottom-0 h-72 w-full flex flex-col justify-between backdrop-blur-2xl bg-black bg-opacity-50 p-4">
+        <div className="flex-grow">
+          <h1 className="text-2xl font-semibold text-gray-200">{title}</h1>
+          <div className="mt-4 text-gray-300">{description}</div>
+          <div className="flex flex-wrap mt-4">
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className="bg-blue-600 text-xs flex m-1 px-3 py-1 items-center justify-center bg-opacity-80 hover:bg-opacity-40 rounded-xl text-white"
+              >
+                {feature}
+              </div>
+            ))}
           </div>
-          <div className="flex justify-end">
-            <button
-              onClick={handleButtonClick}
-              className="px-6 py-3 bg-blue-600 text-white cursor-pointer rounded-full hover:bg-blue-700 transition-colors duration-300"
-            >
-              View Project
-            </button>
-          </div>
+        </div>
+        <div className="flex items-center w-full justify-center">
+          <button
+            onClick={handleButtonClick}
+            className="px-6 py-3 backdrop-blur-2xl  hover:bg-white hover:text-black bg-black w-fit  rounded-full font-bold     transition-all duration-300 text-white cursor-pointer  ease-in-out transform hover:scale-105"
+          >
+            View Project
+          </button>
         </div>
       </div>
     </div>
